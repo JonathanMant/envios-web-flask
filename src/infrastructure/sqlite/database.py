b68cfd90650a8594e45c715088226d9cf22809e1
JonathanMant <jonathan_mantilla@hotmail.com>
2023-11-15 20:17:42 -0500

    fix: change database

--- src/infrastructure/sqlite/database.py
+++ src/infrastructure/sqlite/database.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import URL
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/sqlite.db"
+
+url = URL.create(
+    drivername='postgresql+psycopg2',
+    username='test-db',
+    host='postgres',
+    database='test-bulk',
+    password='test+4304+',
+    port=5432
+)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={
-        "check_same_thread": False,
-    },
+    url=url
 )
 
 SessionLocal = sessionmaker(
